981ab251214befa19f7ada34e92dfb9e784a5d5d
Samsing Pedrals, Francisca (A&F, Hobart) <Francisca.Samsingpedrals@csiro.au>
2018-08-29 12:03:36 +1000

    "Adding script that cleans metadata"

---
+++ Scripts/cleaning_metadata.R
@@ -0,0 +1,35 @@
+## Open Metadata and clean ## 
+
+# tidyverse 1.2.1
+library(tidyverse)
+
+samples <- read.table('metadata.txt', sep = " ")
+samples <- as.tibble(samples)
+samples
+
+colnames(samples) <- c('sample_id','total_read_bases_(bp)','total_reads', 'GC','AT','Q20','Q30')
+
+## DATA WITHOUT SAMPLE IDs ##
+samples_data <- samples[,2:7]
+samples_data
+
+## SAMPLE IDs without Neg controls
+samples_id_virus <- samples[4:15,1]
+samples_id_virus
+
+samples_id_virus<- samples_id_virus %>% extract(col = sample_id, into = c("treatment", "hours_post_infection", "replicate"), 
+                             regex = regex("(\\D+)(\\d+).+(\\d)"))
+
+# Neg controls IDs 
+samples_id_neg <- samples[1:3,1]
+samples_id_neg <- sample_id_neg %>% extract(col = sample_id, into = c("treatment", "hours_post_infection", "replicate"), 
+                          regex = regex("(\\D+)(.+)(\\d)")) %>% 
+                  mutate_if(is.character, str_replace_all, pattern = "R", replacement = "0")
+
+samples_id_new <- rbind(samples_id_neg, samples_id_virus)
+samples_id_new
+
+samples_metadata <- cbind(samples_id_new, samples_data)   
+samples_metadata
+
+write.csv(samples_metadata, "metadata_clean.csv")
