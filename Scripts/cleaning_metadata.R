0f2d07da57b8625e4ac3ac00572a0d2a65c83586
Samsing Pedrals, Francisca (A&F, Hobart) <Francisca.Samsingpedrals@csiro.au>
2018-08-31 14:19:52 +1000

    Added the md5sum to METADATA.csv

--- Scripts/cleaning_metadata.R
+++ Scripts/cleaning_metadata.R
@@ -2,41 +2,33 @@
 # tidyverse 1.2.1
 library(tidyverse)
 
-files <- list.files(path = '/OSM/CBR/AF_POMV/work/POMV_RNA_seq/Data/')
-files <- files[1:30]
 
-files <- as.tibble(files)
-colnames(files) <- 'sample_id'
+raw_data <- read.csv('Raw_data_file_from_provider.txt', sep = " ", header = TRUE)
+raw_data <- raw_data[,1:3]
+colnames(raw_data) <- c('sample_id', 'file_size', 'md5sum')
+raw_data <- as.tibble(raw_data)
 
-negatives <- files[13:18,1]
-pomv <- files[19:30,1]
-isav <- files[1:12,1]
+negatives <- raw_data[1:6,1:3]
+viruses <- raw_data[7:30,1:3]
 
 
-pomv_new <- pomv %>% extract(col = sample_id, into = c("treatment", "hpi", "replicate", "direction"), 
-                             regex = regex("([A-Z]{4})(\\d+).+(\\d)_(\\d)"))
+viruses_new <- viruses %>% extract(col = sample_id, into = c("treatment", "hpi", "replicate", "read"), 
+                             regex = regex("([A-Z]{4})(\\d+).+(\\d)_(\\d)")) 
 
-isav_new <- isav %>% extract(col = sample_id, into = c("treatment", "hpi", "replicate", "direction"), 
-                             regex = regex("([A-Z]{4})(\\d+).+(\\d)_(\\d)"))
-
-negatives_new <- negatives %>% extract(col = sample_id, into = c("treatment", "hpi", "replicate", "direction"), 
+negatives_new <- negatives %>% extract(col = sample_id, into = c("treatment", "hpi", "replicate", "read"), 
                                  regex = regex("(\\D+)control(R)(\\d)_(\\d)")) %>%
                                 mutate_if(is.character, str_replace_all, pattern = "R", replacement = "0")
 
 
-samples_metadata <- rbind(isav_new, negatives_new, pomv_new)
+samples_metadata <- rbind(negatives_new, viruses_new)
 samples_metadata
 
-samples_metadata_final <- cbind(files, samples_metadata)
-
+samples_metadata_final <- cbind(raw_data[,1], samples_metadata[,5:6], samples_metadata[,1:4])
+samples_metadata_final
 str(samples_metadata_final)
 
 
-
-
-
-
-write.table(samples_metadata_final, "METADATA.txt", row.names = F, sep="\t", quote=FALSE)
+write.table(samples_metadata_final, "METADATA.csv", row.names = F, sep=",", quote=FALSE)
 
 
 
