383e377a57bfc8946ecdff0eee5e8347def868c3
Samsing Pedrals, Francisca (A&F, Hobart) <Francisca.Samsingpedrals@csiro.au>
2018-08-31 15:51:52 +1000

    Updated get_metada.sh

--- Scripts/get_metadata.sh
+++ Scripts/get_metadata.sh
@@ -10,6 +10,7 @@
 #***************************************************************#
 
 
-files=/OSM/CBR/AF_POMV/work/POMV_RNA_seq/Data/*.fastq.gz
 
-for file in $files; do zcat $file | head -n 1; done
+filenames="$(cut -d , -f 1 METADATA.csv | grep -v sample_id)"
+
+for f in $filenames;do echo $f; zcat /OSM/CBR/AF_POMV/work/POMV_RNA_seq/Data/*.fastq.gz | head -n 1 ; done
