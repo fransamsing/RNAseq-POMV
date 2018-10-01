9ba13bfa9d58cdda16e2877f44d6d1cd8775f693
Samsing Pedrals, Francisca (A&F, Hobart) <Francisca.Samsingpedrals@csiro.au>
2018-10-01 16:33:44 +1000

    Removed .swap file

--- Scripts/md5sum_check.sh
+++ Scripts/md5sum_check.sh
@@ -4,7 +4,7 @@
 cut -d , -f3 ../METADATA.csv | grep -v md5sum| sort > ../Results/md5sum_from_provider.csv
 
 # Run md5sum command on data files in Bowen, 
-# cut and sort teh column with md5sum data and save as an intermediate file
+# cut and sort the column with md5sum data and save as an intermediate file
 md5sum /OSM/CBR/AF_POMV/work/POMV_RNA_seq/Data/*.gz | cut -d ' ' -f1 | sort > ../Results/md5sum_from_data.csv
 
 # Check the difference in two files
